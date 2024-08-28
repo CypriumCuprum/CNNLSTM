77012d7cc5f5d44d051b7c926314fa9625f57fc7
CypriumCuprum <riverflowsinyou1412@gmail.com>
2024-08-28 10:14:25 +0700

    fix_class

--- train_eeg.py
+++ train_eeg.py
@@ -14,11 +14,11 @@ warnings.filterwarnings("ignore")
 
 device = ("cuda" if torch.cuda.is_available() else "cpu")
 
-# train_set = EEG_ImageDataset(r"/kaggle/input/eeg-signal-image-time-frequency/spectrogram")
-# valid_set = EEG_ImageDataset(r"/kaggle/input/eeg-signal-image-time-frequency/spectrogram")
+train_set = EEG_ImageDataset(r"/kaggle/input/eeg-signal-image-time-frequency/spectrogram")
+valid_set = EEG_ImageDataset(r"/kaggle/input/eeg-signal-image-time-frequency/spectrogram")
 
-train_set = EEG_ImageDataset(f"data/spectrogram", numclass=40)
-valid_set = EEG_ImageDataset(f"data/spectrogram", numclass=40)
+# train_set = EEG_ImageDataset(f"data/spectrogram", numclass=40)
+# valid_set = EEG_ImageDataset(f"data/spectrogram", numclass=40)
 
 train_loader = DataLoader(dataset=train_set, batch_size=8, pin_memory=True, shuffle=True)
 valid_loader = DataLoader(dataset=valid_set, batch_size=8, pin_memory=True, shuffle=True)
